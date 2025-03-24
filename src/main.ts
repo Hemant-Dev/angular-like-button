57b4df519146dbe5e1f58ee37a5b82c29a047371
Hemant Dev <phemant092@gmail.com>
2025-03-24 12:00:14 +0530

    Add @angular/elements package and implement LikeButton component in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, createApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { createCustomElement } from '@angular/elements';
+import { LikeButtonComponent } from './app/Components/like-button/like-button.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+createApplication(appConfig)
+  .then((app) => {
+    const LikeButton = createCustomElement(LikeButtonComponent, { injector: app.injector });
+    customElements.define('like-button', LikeButton);
+  })
+  .catch((err) => console.error(err)); 
